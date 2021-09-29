7c7ac3d18e0a108e0fe2864df719b47393f37e98
Mahmudul Hasan <17101120@uap-bd.edu>
2021-09-29 06:57:05 +0600

    inport bootstrap card

--- news/src/App.js
+++ news/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Row } from 'react-bootstrap';
+import News from './components/News/News';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row xs={1} md={3} className="g-4">
+        <News></News>
+      </Row>
     </div>
   );
 }
